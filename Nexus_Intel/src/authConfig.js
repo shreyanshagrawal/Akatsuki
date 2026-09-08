c323dd9011493e8c842e5146ea15746e589168ef
HridayRaj02 <hridayrajsinghkrishnawat@gmail.com>
2026-09-08 12:55:14 +0530

    password update login

--- Nexus_Intel/src/authConfig.js
+++ Nexus_Intel/src/authConfig.js
@@ -1,5 +1,5 @@
 // Demo-only client-side credentials. Replace this with a server-backed auth flow before deployment.
 export const DEMO_CREDENTIALS = {
   email: 'investigator.roy@cbi.gov.in',
-  password: 'Nexus@2026',
+  password: '1234',
 }
